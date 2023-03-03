c70d246ec8970ef2e4412a70d855e075dfb0acf3
Siddharth Bhosale <salesforcenewvision@gmail.com>
2023-03-03 17:27:08 +0530

    looping

---
+++ force-app/main/default/lwc/looping/looping.js
@@ -0,0 +1,24 @@
+import { LightningElement } from 'lwc';
+
+export default class Looping extends LightningElement {
+carList = ["Ford" , "Maruti","Ferrari"]
+
+ceoList = [
+    {
+        id:1,
+        company:"Google",
+        name:"Sundar"
+    },
+    {
+        id:2,
+        company:"MSFT",
+        name:"Satya"
+    },
+    {
+        id:3,
+        company:"FB",
+        name:"Mark"
+    }
+]
+
+}
