5890a1748b172305710b14a1f779a33e68979323
Siddharth Bhosale <salesforcenewvision@gmail.com>
2023-03-04 08:30:26 +0530

    QuizzApp Finish

--- force-app/main/default/lwc/quizApp/quizApp.js
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -2,8 +2,10 @@ import { LightningElement } from 'lwc';
 
 export default class QuizApp extends LightningElement {
     selected={
-
+   
     }//for storing value
+    correctAnswer=0 //To show the no of correct answer
+    isSubmitted = false //use to show the result
     myQuestions=[
 
        {
@@ -38,7 +40,12 @@ export default class QuizApp extends LightningElement {
        }
 
     ]
-
+    get allNotSelected(){
+        return !(Object.keys(this.selected).length === this.myQuestions.length)
+    }
+    get isScoredFull(){
+        return `slds-text-heading_large  ${this.myQuestions.length === this.correctAnswer? 'slds-text-color_success':'slds-text-color_error'}`
+    }
     changeHandler(event){
         console.log("name", event.target.name)
         console.log("value", event.target.value)
@@ -48,10 +55,16 @@ export default class QuizApp extends LightningElement {
         this.selected={...this.selected, [name]:value}
         //this.selected={"Question1":"b"}
     }
-    submitHandler(){
-        
+    submitHandler(event){
+        event.preventDefault()
+       let correct= this.myQuestions.filter(item=>this.selected[item.id] === item.correctAnswer)
+       this.correctAnswer = correct.length
+       this.isSubmitted = true
+       console.log("this.correctAnswer",this.correctAnswer)
     }
     resetHandler(){
-
+        this.selected = {}
+        this.correctAnswer=0
+        this.isSubmitted = false
     }
 }
