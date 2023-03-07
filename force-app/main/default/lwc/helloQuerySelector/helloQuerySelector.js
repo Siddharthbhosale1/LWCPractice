757b50b582e84b8be676541288e6cefc6de26b99
Siddharth Bhosale <salesforcenewvision@gmail.com>
2023-03-07 06:51:43 +0530

    QuerySelector Updates

--- force-app/main/default/lwc/helloQuerySelector/helloQuerySelector.js
+++ force-app/main/default/lwc/helloQuerySelector/helloQuerySelector.js
@@ -1,9 +1,20 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloQuerySelector extends LightningElement {
+    userName = ["John" , "Smith" , "Jenny"]
     fetchDetailHandler(){
         const elem = this.template.querySelector('h1')
+        elem.style.border="1px solid red";
         console.log(elem.innerText)
+        const userElement = this.template.querySelectorAll('.name');
+        
+        Array.from(userElement).forEach(item=>{
+            console.log(item.innerText)
+        })
+
+        ////lwc:dom="manual" demo
+        const childElem = this.template.querySelector('.child')
+        childElem.innerHTML = '<p>hey im a child</p>'
     }
 
 }
