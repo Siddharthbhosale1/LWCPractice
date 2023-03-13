dea49b095b1af26eef09bb860834321b0453a8c9
Siddharth Bhosale <salesforcenewvision@gmail.com>
2023-03-13 13:07:45 +0530

    Render()

--- force-app/main/default/lwc/renderMethod/renderMethod.js
+++ force-app/main/default/lwc/renderMethod/renderMethod.js
@@ -3,8 +3,17 @@ import signIn from './signIn.html'
 import signUp from './signUp.html'
 import renderMethod from './renderMethod.html'
 export default class RenderMethod extends LightningElement {
-
+    selectedBtn
     render(){
-        return renderMethod
+        return this.selectedBtn === 'Signup' ? signUp :
+            this.selectedBtn === 'Signin' ? signIn :
+            renderMethod
+    }
+    handleClick(event){
+        this.selectedBtn = event.target.label
+    }
+
+    submitHandler(event){
+        console.log(`${event.target.label} Successfully`)
     }
 }
