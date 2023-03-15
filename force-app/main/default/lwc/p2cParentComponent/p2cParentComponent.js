760331ecfddc6454355c5aeec0325b584dbf3c60
Siddharth Bhosale <salesforcenewvision@gmail.com>
2023-03-15 11:57:00 +0530

    3/15/23-2

--- force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class P2cParentComponent extends LightningElement {}
+export default class P2cParentComponent extends LightningElement {
+   
+}
