5e6527b46c468ac8dfecbdcaf217aac7a6701f58
Siddharth Bhosale <salesforcenewvision@gmail.com>
2023-03-17 19:25:06 +0530

    p2cEnd

--- force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -24,6 +24,9 @@ export default class P2cParentComponent extends LightningElement {
     changeHandler(event){
         this.percentage= event.target.value
     }
+    handleClick(){
+        this.template.querySelector('c-p2c-slider-component').resetSlider()
+    }
    
 }
 
