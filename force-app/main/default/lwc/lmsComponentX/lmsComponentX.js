7c1f17aec75ae1d21c5e6c91808e9d8820859674
Siddharth Bhosale <salesforcenewvision@gmail.com>
2023-03-22 10:19:45 +0530

    LMS

--- force-app/main/default/lwc/lmsComponentX/lmsComponentX.js
+++ force-app/main/default/lwc/lmsComponentX/lmsComponentX.js
@@ -1,10 +1,18 @@
 import { LightningElement,wire } from 'lwc';
-import SAMPLEMC from "salesforce/messageChannel/SampleMessageChannel__c"
-import { subscribe, MessageContext } from 'lightning/messageService';
+import SAMPLEMC from "@salesforce/messageChannel/SampleMessageChannel__c"
+import { subscribe, MessageContext, APPLICATION_SCOPE } from 'lightning/messageService';
 export default class LmsComponentX extends LightningElement {
     @wire(MessageContext)
         context
-
-        subscribe()
+        connectedCallback(){
+            this.subscribeMessage()
+        }
+    subscribeMessage(){
+        subscribe( this.context,SAMPLEMC, (message)=>{this.handleMessage(message)},{scope:APPLICATION_SCOPE})
+    }
+    
+    handleMessage(message){
+        this.recievedMessage = message.lmsData.value?message.lmsData.value:'No Message Published'
+    }
     
 }
