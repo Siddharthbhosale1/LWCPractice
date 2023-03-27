a381daa08a268ce11a6fe21fca4b3c9fffeaa435
Siddharth Bhosale <salesforcenewvision@gmail.com>
2023-03-27 12:21:10 +0530

    contentFiles

---
+++ force-app/main/default/lwc/contentAssetFile/contentAssetFile.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+import FILE from '@salesforce/contentAssetUrl/Offer_Lettercompletedpdf'
+export default class ContentAssetFile extends LightningElement {
+    file=FILE
+}
