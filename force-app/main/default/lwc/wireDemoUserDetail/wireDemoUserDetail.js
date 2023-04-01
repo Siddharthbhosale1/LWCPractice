dad0bbe017d93505922c95c296a81844cfd1b7cf
Siddharth Bhosale <salesforcenewvision@gmail.com>
2023-04-01 06:42:21 +0530

    recordEdit

---
+++ force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
@@ -0,0 +1,21 @@
+import { LightningElement , wire } from 'lwc';
+import {getRecord} from 'lightning/uiRecordApi'
+import Id from '@salesforce/user/Id'
+export default class WireDemoUserDetail extends LightningElement {
+    userId = Id
+    //0055j0000051nhlAAA
+
+   // @wire(adapter , {adapterConfig})
+   // propertyoffunction
+   userDetails
+@wire(getRecord, {recordId:'0055j0000051nhlAAA',fields:['User.Name', 'User.Email']})
+userDetailHandler({data,error}){
+    if(data){
+       this.userDetails = data.fields 
+    }
+    if(error){
+        console.error(error)
+    }
+  }
+
+}
