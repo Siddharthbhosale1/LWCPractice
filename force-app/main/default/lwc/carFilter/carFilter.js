93ff6bf4f88eab21e64b8485ed0ca8f2ec095e0e
Siddharth Bhosale <salesforcenewvision@gmail.com>
2023-04-09 18:34:53 +0530

    picklists

--- force-app/main/default/lwc/carFilter/carFilter.js
+++ force-app/main/default/lwc/carFilter/carFilter.js
@@ -1,10 +1,33 @@
-import { LightningElement } from 'lwc';
+import { LightningElement , wire} from 'lwc';
+import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import CAR_OBJECT from '@salesforce/schema/cars__c'
+import CATEGORY_FIELD from '@salesforce/schema/cars__c.Category__c'
+import MAKE_FIELD from '@salesforce/schema/cars__c.Make__c'
 
+const CATEGORY_ERROR = 'Error loading categories'
+const MAKE_ERROR = 'Error loading make types'
 export default class CarFilter extends LightningElement {
     filters={
         searckKey:'',
         maxPrice:999999
     }
+
+    /***fetching Category picklist */
+    categoryError=CATEGORY_ERROR
+    makeError=MAKE_ERROR
+    @wire(getObjectInfo,{objectApiName:CAR_OBJECT})
+    carObjectInfo
+
+    @wire(getPicklistValues,{
+        recordTypeId:'$carObjectInfo.data.defaultRecordTypeId',
+        fieldApiName:CATEGORY_FIELD
+    })categories
+
+    @wire(getPicklistValues,{
+        recordTypeId:'$carObjectInfo.data.defaultRecordTypeId',
+        fieldApiName:MAKE_FIELD
+    })makeType
     handleSearchKeyChange(){}
     handleMaxPriceChange(){}
+    handleCheckbox(){}
 }
